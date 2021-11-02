315e5c9d30fa158249e34b35e248b5659de4ba7b
ivanfigueiredo <77892424+ivanfigueiredo@users.noreply.github.com>
2021-11-02 16:54:06 -0300

    Update AuthController.js

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -1,97 +1,86 @@
 const User = require('../Model/User');
 
 module.exports = {
-    userUpdate: async (req, res) => {        
-        let userId = await req.params.userId.split("="); 
-        let {email, password, username} = await req.body;                
-                  
-        const users = await User.find();
-        let id = '';
-
-        for(let i=0; i < users.length; i++){
-            console.log(users[i].login.uuid);
-            console.log(userId[1]);
-            if(users[i].login.uuid === userId[1]){
-                id = users[i]._id;                
+    userUpdate: async (req, res) => { 
+        try {
+            let userId = await req.params.userId.split("="); 
+            let {email, password, username} = await req.body;                
+                    
+            const user = await User.findOne({'login.uuid': userId[1]});        
+            
+            if(!user){
+                res.status(401).send('UUID Invalido!');
+                return;
+            }                        
+            
+            let updates = {
+                email: `${user.email}`,
+                login: {
+                    uuid: `${user.login.uuid}`,
+                    username: `${user.login.username}`,
+                    password: `${user.login.password}`,
+                    salt: `${user.login.salt}`,
+                    md5: `${user.login.md5}`,
+                    shal: `${user.login.shal}`,
+                    sha256: `${user.login.sha256}`
+                }
             }
-        }      
-        
-        if(!id){
-            res.json({error: 'ID inválido!'});
-            return;
-        }
-                
-        const user = await User.findOne({_id: id});
-        
-        let updates = {
-            email: `${user.email}`,
-            login: {
-                uuid: `${user.login.uuid}`,
-                username: `${user.login.username}`,
-                password: `${user.login.password}`,
-                salt: `${user.login.salt}`,
-                md5: `${user.login.md5}`,
-                shal: `${user.login.shal}`,
-                sha256: `${user.login.sha256}`
+            if(email){
+                updates.email = email;            
             }
-        }
-        if(email){
-            updates.email = email;            
-        }
 
-        if(username){
-            updates.login.username = username;
-        }
+            if(username){
+                updates.login.username = username;
+            }
 
-        if(password){
-            updates.login.password = password
-        }
-        
-        await User.findOneAndUpdate({_id: id}, {$set: updates});                                            
-        res.json({Ok: true});                         
+            if(password){
+                updates.login.password = password
+            }
+            
+            await User.findOneAndUpdate({_id: user._id}, {$set: updates});                                            
+            res.status(200).send('OK');   
+
+        } catch (err){
+            console.error(err);
+            res.status(501).send(err)
+        }                                   
         
     },
 
-    info: async (req, res) => {        
-        let userId = await req.params.userId.split("="); 
-                                                        
-        let users = await User.paginate();                                                                   
-        for(let i=0; i < users.docs.length; i++){       
-            console.log(users.docs[i].login.uuid)                                                        
-            console.log(userId[1])
-            if(users.docs[i].login.uuid === userId[1]){
-                res.json({user: users.docs[i]});
+    info: async (req, res) => {
+        try {
+            let userId = await req.params.userId.split("=");                                                         
+            const user = await User.findOne({'login.uuid': userId[1]}); 
+            
+            if(!user){
+                res.status(401).send('UUID Invalido!');
                 return;
-            }
-        }                                    
-        res.json({error: "ID inválido!"});                                      
-                                       
+            }                                    
+            
+            res.status(200).send({user});
+
+        } catch (err) {
+            console.error(err);
+            res.status(501).send(err);
+        }              
     },
 
     remove: async (req, res) => {
-        try{
+        try {
             let userId = await req.params.userId.split("=");
-            const users = await User.find();
-            let id = '';
-
-            for(let i=0; i < users.length; i++){
-                console.log(users[i].login.uuid);
-                console.log(userId[1]);
-                if(users[i].login.uuid === userId[1]){
-                    id = users[i]._id;                
-                }
-            }
-
-            if(!id){
-                res.json({error: 'ID inválido!'});                
+            const user = await User.findOne({'login.uuid': userId[1]});             
+            
+            if(!user){
+                res.status(401).send('UUID Invalido!');               
                 return;
             }
             
-            await User.findOneAndRemove({_id: id});            
-            
-            res.json({remove: true});
+            await User.findOneAndRemove({_id: user._id});                        
+            res.status(200).send('OK');
+
         } catch (err){
             console.error(err);
+            res.status(500).send(err);
         }
     },
 
@@ -105,6 +94,7 @@ module.exports = {
             
             const users = await User.paginate({}, options);
             res.json({users});
+
         } catch (err){
             console.error(err);
             res.status(500).send(err);
